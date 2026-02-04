634030a90887b4766201f87e2e3e422df25a2923
syedaabid1995 <syedaabid1995@gmail.com>
2026-02-04 23:35:31 +0530

    Test 6

--- admin/app/login/page.tsx
+++ admin/app/login/page.tsx
@@ -230,7 +230,7 @@ export default function LoginPage() {
               </a>
             </p>
             <p className="text-xs text-gray-400 mt-2">
-              © 2026 SchoolApp by TechStage IT. All rights reserved.
+              © 2026 SchoolApp by TechStage IT. All rights reserved
             </p>
           </div>
         </div>
